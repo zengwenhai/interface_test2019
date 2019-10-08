f1797bf1c0bc04a9b39e4d67ae2adc081d83dbeb
374448636@qq.com <374448636@qq.com>
2019-10-08 18:22:04 +0800

    执行脚本添加sys

--- main/runTest.py
+++ main/runTest.py
@@ -3,7 +3,7 @@ import sys
 import os
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
-sys.path.append(curPath)
+sys.path.append(rootPath)
 print(rootPath)
 from config.getPath import getpath
 from utils.HTMLTestRunner_PY3 import HTMLTestRunner
