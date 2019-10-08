0fe5df36fc87e3c50dbd83c1c673ada5899e0574
374448636@qq.com <374448636@qq.com>
2019-10-08 18:17:49 +0800

    执行脚本添加sys

--- main/runTest.py
+++ main/runTest.py
@@ -1,9 +1,10 @@
 import unittest
 import sys
 import os
-curPath = os.path.abspath(os.path.dirname(__file__))
-rootPath = os.path.split(curPath)[0]
-sys.path.append(rootPath)
+curPath = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+# rootPath = os.path.split(curPath)[0]
+sys.path.append(curPath)
+print(curPath)
 from config.getPath import getpath
 from utils.HTMLTestRunner_PY3 import HTMLTestRunner
 from BeautifulReport import BeautifulReport
